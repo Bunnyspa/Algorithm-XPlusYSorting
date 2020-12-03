2b8d58f8b2695344b452ce8bbfe8f11e63ba43be
Bunnyspa <bunnyspa242@gmail.com>
2020-12-03 14:54:59 +0900

    Unnecessary comment removal

--- src/xplusysorting/App.java
+++ src/xplusysorting/App.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package xplusysorting;
 
 import java.util.List;
 import java.util.Random;
 
-/**
- *
- * @author Bunnyspa
- */
 public class App {
 
     public static void main(String[] args) throws Exception {
--- src/xplusysorting/Vertex.java
+++ src/xplusysorting/Vertex.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package xplusysorting;
 
 import java.util.Collection;
 
-/**
- *
- * @author Bunnyspa
- */
 class Vertex {
 
     private Edge right, down;
