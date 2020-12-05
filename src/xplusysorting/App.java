0cb181463c1e22333c54027d2315abfa56565567
Bunnyspa <bunnyspa242@gmail.com>
2020-12-06 00:27:50 +0900

    Update App.java

--- src/xplusysorting/App.java
+++ src/xplusysorting/App.java
@@ -6,20 +6,30 @@ import java.util.Random;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        testTime();
+        test();
     }
 
-    private static void testTime() throws Exception {
-        int size = 16;
-        System.out.println("Size: " + size);
-        long start = System.nanoTime();
-        int[] x = step(size, 7, 0);
-        int[] y = step(size, 11, 0);
-        XPlusYSorting algorithm = new XPlusYSorting(x, y);
-        List<Pair> out = algorithm.sort();
-        System.out.println(verify(out));
-        long elapsed = System.nanoTime() - start;
-        System.out.println((double) elapsed / 1000000000);
+    private static void test() throws Exception {
+        for (int size : new int[]{100, 200}) {
+            System.out.println("Size: " + size);
+            long start = System.nanoTime();
+            int[] x = step(size, 7,0);
+            int[] y = step(size, 11,0);
+            XPlusYSorting algorithm = new XPlusYSorting(x, y);
+            List<Pair> out = algorithm.sort();
+            System.out.println(verify(out));
+            long elapsed = System.nanoTime() - start;
+            System.out.println((double) elapsed / 1000000000);
+            System.out.println("--------------------------------------------------");
+        }
+    }
+
+    private static int[] repeat(int size, int number) {
+        int[] out = new int[size];
+        for (int i = 0; i < size; i++) {
+            out[i] = number;
+        }
+        return out;
     }
 
     private static int[] step(int size, int step, int offset) {
